113a15d5f50e7d36a555e9fb05ecbddb6170b302
Gustavo Gondim <gustavospgondim@gmail.com>
2020-12-14 10:24:23 -0300

    first working version

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue} from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false
 
 new Vue({
